e93b083d349a51c9df67f3f255be6977818accc6
CarlosDeMoraGil <carlosdemoragil825@gmail.com>
2026-03-26 13:18:17 +0100

    NavIntent and InformationFragment updated

---
+++ app/src/main/java/com/carlosdmg/ecoclinic/app/presentation/NavIntent.kt
@@ -0,0 +1,23 @@
+package com.carlosdmg.ecoclinic.app.presentation
+
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.core.net.toUri
+import com.carlosdmg.ecoclinic.R
+
+class NavIntent (private val context: Context) {
+
+    fun openPhoneDial(phoneNumber: Int) {
+        try {
+            val parsedPhoneNumber = "tel:${context.getString(phoneNumber)}".toUri()
+            val intent = Intent(Intent.ACTION_DIAL, parsedPhoneNumber).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            }
+            context.startActivity(intent)
+        } catch (e: Exception) {
+            Log.d("@dev", context.getString(R.string.app_intent_error))
+        }
+    }
+
+}
--- app/src/main/java/com/carlosdmg/ecoclinic/feature/information/presentation/InformationFragment.kt
+++ app/src/main/java/com/carlosdmg/ecoclinic/feature/information/presentation/InformationFragment.kt
@@ -5,13 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.carlosdmg.ecoclinic.app.MapView
+import com.carlosdmg.ecoclinic.R
+import com.carlosdmg.ecoclinic.app.presentation.NavIntent
 import com.carlosdmg.ecoclinic.databinding.FragmentInformationBinding
+import com.google.android.gms.maps.SupportMapFragment
 
 class InformationFragment : Fragment() {
 
     private var _binding: FragmentInformationBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var navIntent: NavIntent
+
+    private val map = MapView()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,10 +31,35 @@ class InformationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        navIntent = NavIntent(requireContext())
+
+        setUpMap()
+        setUpBinding()
+
     }
 
+    fun setUpMap(){
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(map)
+    }
+
+    fun setUpBinding(){
+        binding.apply {
+
+            inFrPhoneButton.setOnClickListener {
+                navigateToDial(R.string.in_fr_phone_number)
+            }
+
+        }
+    }
+
+    fun navigateToDial(phoneNumber: Int){
+        navIntent.openPhoneDial(phoneNumber)
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        _binding = null
     }
 }
