d77a1dcc594667edb7fdff6ed6157bf3db81c561
CarlosDeMoraGil <146753433+CarlosDeMoraGil@users.noreply.github.com>
2026-04-13 11:32:04 +0200

    Pantalla de Información (#4)
    * Gitignore modified
    
    * Implemented Google Maps, with dependencies and builds
    
    * Added values and ics
    
    * NavIntent and InformationFragment updated
    
    * res files
    
    * Review coded
    
    * no clue

---
+++ app/src/main/java/com/carlosdmg/ecoclinic/app/MapView.kt
@@ -0,0 +1,29 @@
+package com.carlosdmg.ecoclinic.app
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapView : OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val coords = convertLatLong("40.293901356464914", "-4.580089770692452")
+        mMap.addMarker(
+            MarkerOptions().position(coords).title("Consulta de la Dra. Isabel Gil Carrera")
+        )
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coords, 14f))
+    }
+
+    fun convertLatLong(lat: String, long: String): LatLng {
+        return LatLng(lat.toDouble(), long.toDouble())
+
+    }
+
+}
