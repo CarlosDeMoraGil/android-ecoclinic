106c2b0aa2051f99a7420bdf274dc00e634e7172
CarlosDeMoraGil <carlosdemoragil825@gmail.com>
2026-05-07 16:33:29 +0200

    DI files

---
+++ app/src/main/java/com/carlosdmg/ecoclinic/feature/user/di/UserModule.kt
@@ -0,0 +1,10 @@
+package com.carlosdmg.ecoclinic.feature.user.di
+
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Module
+
+@Module
+@ComponentScan("com.carlosdmg.ecoclinic.feature.user")
+class UserModule {
+
+}
